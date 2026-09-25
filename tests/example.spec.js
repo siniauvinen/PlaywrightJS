749daf6dc9f46760f820166ef92678a2a4e0363b
Sini Auvinen <72907985+siniauvinen@users.noreply.github.com>
2026-09-25 13:43:51 +0300

    adding secrets to the test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,7 +3,8 @@ test('test', async ({ page }) => {
   await page.goto('https://automationplayground.com/crm/');
   await expect(page).toHaveTitle(/Customer Service/);
   await page.getByRole('link', { name: 'Sign In' }).click();
-  await page.getByRole('textbox', { name: 'Enter email' }).fill('myemail@email.fi');
-  await page.getByRole('textbox', { name: 'Password' }).fill('mypassword');
+  await page.getByRole('textbox', { name: 'Enter email' }).fill(process.env.SF_USERNAME);
+  await page.getByRole('textbox', { name: 'Password' }).fill(process.env.SF_PASSWORD);
   await page.getByRole('button', {name: 'Submit'}).click();
+  console.log(process.env.SF_USERNAME);
 });
