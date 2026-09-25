8955aac16b8d6690f504a05a81d89107f0b1f9d1
Sini Auvinen <72907985+siniauvinen@users.noreply.github.com>
2026-09-25 14:07:28 +0300

    .env added

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,10 +1,11 @@
 import { test, expect } from '@playwright/test';
+import 'dotenv/config';
 test('test', async ({ page }) => {
   await page.goto('https://automationplayground.com/crm/');
   await expect(page).toHaveTitle(/Customer Service/);
   await page.getByRole('link', { name: 'Sign In' }).click();
-  await page.getByRole('textbox', { name: 'Enter email' }).fill(process.env.SF_USERNAME);
-  await page.getByRole('textbox', { name: 'Password' }).fill(process.env.SF_PASSWORD);
+  await page.getByRole('textbox', { name: 'Enter email' }).fill(process.env.CRM_USERNAME);
+  await page.getByRole('textbox', { name: 'Password' }).fill(process.env.CRM_PASSWORD);
   await page.getByRole('button', {name: 'Submit'}).click();
-  console.log(process.env.SF_USERNAME);
+  console.log(process.env.CRM_USERNAME);
 });
